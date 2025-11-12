aae2e760ed5327c33ef2921b4cf4d2554c7b93a1
alecroessler <alec_roessler1@baylor.edu>
2025-11-12 16:55:15 -0600

    Update mcs_top_sampler.sv

--- Experiment_16.8.1/hdl/mcs_top_sampler.sv
+++ Experiment_16.8.1/hdl/mcs_top_sampler.sv
@@ -1,176 +1,108 @@
-// specifiy I/O core parameters in this level 
-
-`include "chu_io_map.svh"
-module mmio_sys_sampler
-#(
-  parameter N_SW = 8,
-            N_LED = 8
-)	
+module mcs_top_sampler
+#(parameter BRG_BASE = 32'hc000_0000)	
 (
-   input logic clk,
-   input logic reset,
-   // FPro bus 
-   input  logic mmio_cs,
-   input  logic mmio_wr,
-   input  logic mmio_rd,
-   input  logic [20:0] mmio_addr, 
-   input  logic [31:0] mmio_wr_data,
-   output logic [31:0] mmio_rd_data,
+   input  logic clk,
+   input  logic reset_n,
    // switches and LEDs
-   input  logic [N_SW-1:0] sw,
-   output logic [N_LED-1:0] led,
+   input  logic [15:0] sw,
+   output logic [15:0] led,
    // uart
    input  logic rx,
    output logic tx,          
    // 4 analog input pair
    input  logic [3:0] adc_p, adc_n,
-   // pwm 
-   output logic [7:0] pwm,
+   // rgb leds (pwm) 
+   output logic [2:0] rgb_led1, rgb_led2,
    // btn
-   input logic [4:0] btn,
+   input  logic [4:0] btn,
    // 8-digit 7-seg LEDs
    output logic [7:0] an,
    output logic [7:0] sseg,
    // spi accelerator
    output logic acl_sclk, acl_mosi,
    input  logic acl_miso,
-   output logic acl_ss,
+   output logic acl_ss_n,
    // i2c temperature sensor  
    output logic tmp_i2c_scl,
    inout  tri tmp_i2c_sda,
    // ps2
    inout  tri ps2d,
    inout  tri ps2c,
-   // ddfs square wave output
-   output  logic  ddfs_sq_wave,
-   // 1-bit dac 
-    output logic  pdm 
+   // nexsys 4 aduio
+   output logic audio_on,audio_pdm,
+   // PMOD JA (divided into top row and bottom row)
+   output logic [4:1] ja_top,
+   output logic [10:7] ja_btm
 );
 
-   //declaration
-   logic [63:0] mem_rd_array;
-   logic [63:0] mem_wr_array;
-   logic [63:0] cs_array;
-   logic [4:0] reg_addr_array [63:0];
-   logic [31:0] rd_data_array [63:0]; 
-   logic [31:0] wr_data_array [63:0];
-   logic [15:0] adsr_env;
+   // declaration
+   logic clk_100M;
+   logic reset_sys;
+   // MCS IO bus
+   logic io_addr_strobe;
+   logic io_read_strobe;
+   logic io_write_strobe;
+   logic [3:0] io_byte_enable;
+   logic [31:0] io_address;
+   logic [31:0] io_write_data;
+   logic [31:0] io_read_data;
+   logic io_ready;
+   // fpro bus 
+   logic fp_mmio_cs; 
+   logic fp_wr;      
+   logic fp_rd;     
+   logic [20:0] fp_addr;       
+   logic [31:0] fp_wr_data;    
+   logic [31:0] fp_rd_data;    
+   // pwm 
+   logic [7:0] pwm; 
+   // ddfs/audio pdm 
+   logic pdm, ddfs_sq_wave;
 
    // body
-   // instantiate mmio controller 
-   chu_mmio_controller ctrl_unit
-   (.clk(clk),
-    .reset(reset),
-    .mmio_cs(mmio_cs),
-    .mmio_wr(mmio_wr),
-    .mmio_rd(mmio_rd),
-    .mmio_addr(mmio_addr), 
-    .mmio_wr_data(mmio_wr_data),
-    .mmio_rd_data(mmio_rd_data),
-    // slot interface
-    .slot_cs_array(cs_array),
-    .slot_mem_rd_array(mem_rd_array),
-    .slot_mem_wr_array(mem_wr_array),
-    .slot_reg_addr_array(reg_addr_array),
-    .slot_rd_data_array(rd_data_array), 
-    .slot_wr_data_array(wr_data_array)
-    );
-  
-   // slot 0: system timer 
-   chu_timer timer_slot0 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S0_SYS_TIMER]),
-    .read(mem_rd_array[`S0_SYS_TIMER]),
-    .write(mem_wr_array[`S0_SYS_TIMER]),
-    .addr(reg_addr_array[`S0_SYS_TIMER]),
-    .rd_data(rd_data_array[`S0_SYS_TIMER]),
-    .wr_data(wr_data_array[`S0_SYS_TIMER])
-    );
-
-   // slot 1: UART 
-   chu_uart #(.FIFO_DEPTH_BIT(8))  uart_slot1 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S1_UART1]),
-    .read(mem_rd_array[`S1_UART1]),
-    .write(mem_wr_array[`S1_UART1]),
-    .addr(reg_addr_array[`S1_UART1]),
-    .rd_data(rd_data_array[`S1_UART1]),
-    .wr_data(wr_data_array[`S1_UART1]), 
-    .tx(tx),
-    .rx(rx)
-    );
-
-   // slot 2: gpo 
-   chu_gpo #(.W(N_LED)) gpo_slot2 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S2_LED]),
-    .read(mem_rd_array[`S2_LED]),
-    .write(mem_wr_array[`S2_LED]),
-    .addr(reg_addr_array[`S2_LED]),
-    .rd_data(rd_data_array[`S2_LED]),
-    .wr_data(wr_data_array[`S2_LED]),
-    .dout(led)
-    );
-
-   // slot 3: gpi 
-   chu_gpi #(.W(N_SW)) gpi_slot3 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S3_SW]),
-    .read(mem_rd_array[`S3_SW]),
-    .write(mem_wr_array[`S3_SW]),
-    .addr(reg_addr_array[`S3_SW]),
-    .rd_data(rd_data_array[`S3_SW]),
-    .wr_data(wr_data_array[`S3_SW]),
-    .din(sw)
-    );
-    
-    // slot 4: reserved for user defined  
-   // chu_gpi #(.W(N_SW)) user_slot4 
-   // (.clk(clk),
-   //  .reset(reset),
-   //  .cs(cs_array[`S4_USER]),
-   //  .read(mem_rd_array[`S4_USER]),
-   //  .write(mem_wr_array[`S4_USER]),
-   //  .addr(reg_addr_array[`S4_USER]),
-   //  .rd_data(rd_data_array[`S4_USER]),
-   //  .wr_data(wr_data_array[`S4_USER]),
-   //  .din(sw)
-   //  );
-   assign rd_data_array[`S4_USER] = 32'h00000000;
+   assign clk_100M = clk;       // 100 MHz external clock
+   assign reset_sys = !reset_n;
+   // audio
+   assign audio_pdm = pdm;
+   assign audio_on = 1'b1;
+   //  rgb leds
+   assign rgb_led2 = pwm[5:3];
+   assign rgb_led1 = pwm[2:0];
+   // PMOD JA  
+   assign ja_top[1] = ddfs_sq_wave;
+   assign ja_top[2] = pdm;
+   assign ja_top[4:3] = pwm[7:6];
+   assign ja_btm = 4'b0000;
    
-
-    
-   // slot 9: spi 
-   spi_core #(.S(1)) spi_slot9 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S9_SPI]),
-    .read(mem_rd_array[`S9_SPI]),
-    .write(mem_wr_array[`S9_SPI]),
-    .addr(reg_addr_array[`S9_SPI]),
-    .rd_data(rd_data_array[`S9_SPI]),
-    .wr_data(wr_data_array[`S9_SPI]),
-    .spi_sclk(acl_sclk),
-    .spi_mosi(acl_mosi),
-    .spi_miso(acl_miso),
-    .spi_ss_n(acl_ss)
+   //instantiate uBlaze MCS
+   cpu cpu_unit (
+    .Clk(clk_100M),                          
+    .Reset(reset_sys),                  
+    .IO_addr_strobe(io_addr_strobe),    
+    .IO_address(io_address),            
+    .IO_byte_enable(io_byte_enable),    
+    .IO_read_data(io_read_data),        
+    .IO_read_strobe(io_read_strobe),    
+    .IO_ready(io_ready),                
+    .IO_write_data(io_write_data),      
+    .IO_write_strobe(io_write_strobe)   
     );
     
+   // instantiate bridge
+   chu_mcs_bridge #(.BRG_BASE(BRG_BASE)) b_unit (.*, .fp_video_cs());
+    
+   // instantiated i/o subsystem
+   mmio_sys_sampler #(.N_SW(16),.N_LED(16)) mmio_unit (
+    .clk(clk),
+    .reset(reset_sys),
+    .mmio_cs(fp_mmio_cs),
+    .mmio_wr(fp_wr),
+    .mmio_rd(fp_rd),
+    .mmio_addr(fp_addr), 
+    .mmio_wr_data(fp_wr_data),
+    .mmio_rd_data(fp_rd_data),
+    .acl_ss(acl_ss_n),          
+    .*  
+   );   
+endmodule    
    
-
-   // assign 0's to all unused slot rd_data signals
-   generate
-      genvar i;
-      for (i=5; i<64; i=i+1) begin
-         if (i != 9)
-            assign rd_data_array[i] = 32'h0;
-      end
-   endgenerate
-endmodule
-
-
-
