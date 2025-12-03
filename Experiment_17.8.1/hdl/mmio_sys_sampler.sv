7ec45f1aea576705ccd297922ea3e06a3e60fa92
alecroessler <alec_roessler1@baylor.edu>
2025-12-03 11:46:53 -0600

    Refactor mmio_sys_sampler module structure
    Refactor mmio_sys_sampler module to streamline slot assignments and remove unused slots.

--- Experiment_17.8.1/hdl/mmio_sys_sampler.sv
+++ Experiment_17.8.1/hdl/mmio_sys_sampler.sv
@@ -127,60 +127,7 @@ module mmio_sys_sampler
     .wr_data(wr_data_array[`S3_SW]),
     .din(sw)
     );
-    
-    // slot 4: reserved for user defined  
-   // chu_gpi #(.W(N_SW)) user_slot4 
-   // (.clk(clk),
-   //  .reset(reset),
-   //  .cs(cs_array[`S4_USER]),
-   //  .read(mem_rd_array[`S4_USER]),
-   //  .write(mem_wr_array[`S4_USER]),
-   //  .addr(reg_addr_array[`S4_USER]),
-   //  .rd_data(rd_data_array[`S4_USER]),
-   //  .wr_data(wr_data_array[`S4_USER]),
-   //  .din(sw)
-   //  );
-   assign rd_data_array[`S4_USER] = 32'h00000000;
-   
-   // slot 5: xadc 
-   chu_xadc_core xadc_slot5 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S5_XDAC]),
-    .read(mem_rd_array[`S5_XDAC]),
-    .write(mem_wr_array[`S5_XDAC]),
-    .addr(reg_addr_array[`S5_XDAC]),
-    .rd_data(rd_data_array[`S5_XDAC]),
-    .wr_data(wr_data_array[`S5_XDAC]),
-    .adc_p(adc_p),
-    .adc_n(adc_n)
-    );
-    
-   // slot 6: pwm 
-    chu_io_pwm_core #(.W(8), .R(10)) pwm_slot6 //
-    (.clk(clk),
-     .reset(reset),
-     .cs(cs_array[`S6_PWM]),
-     .read(mem_rd_array[`S6_PWM]),
-     .write(mem_wr_array[`S6_PWM]),
-     .addr(reg_addr_array[`S6_PWM]),
-     .rd_data(rd_data_array[`S6_PWM]),
-     .wr_data(wr_data_array[`S6_PWM]),
-     .pwm_out(pwm)
-     );
-     
-    // slot 6: pwm 
-     chu_debounce_core #(.W(5), .N(20)) debounce_slot7 //
-     (.clk(clk),
-      .reset(reset),
-      .cs(cs_array[`S7_BTN]),
-      .read(mem_rd_array[`S7_BTN]),
-      .write(mem_wr_array[`S7_BTN]),
-      .addr(reg_addr_array[`S7_BTN]),
-      .rd_data(rd_data_array[`S7_BTN]),
-      .wr_data(wr_data_array[`S7_BTN]),
-      .din(btn)
-      );
+
        
    // slot 8: led mux 
    chu_led_mux_core led_slot8 
@@ -195,22 +142,7 @@ module mmio_sys_sampler
     .sseg(sseg),
     .an(an)
     );
-    
-   // slot 9: spi 
-   chu_spi_core #(.S(1)) spi_slot9 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S9_SPI]),
-    .read(mem_rd_array[`S9_SPI]),
-    .write(mem_wr_array[`S9_SPI]),
-    .addr(reg_addr_array[`S9_SPI]),
-    .rd_data(rd_data_array[`S9_SPI]),
-    .wr_data(wr_data_array[`S9_SPI]),
-    .spi_sclk(acl_sclk),
-    .spi_mosi(acl_mosi),
-    .spi_miso(acl_miso),
-    .spi_ss_n(acl_ss)
-    );
+
     
    // slot 10: i2c 
     chu_i2c_core i2c_slot10 
@@ -225,56 +157,13 @@ module mmio_sys_sampler
      .scl(tmp_i2c_scl),
      .sda(tmp_i2c_sda)
      );
-     
-   // slot 11: ps2 
-    chu_ps2_core #(.W_SIZE(8)) ps2_slot11 
-    (.clk(clk),
-     .reset(reset),
-     .cs(cs_array[`S11_PS2]),
-     .read(mem_rd_array[`S11_PS2]),
-     .write(mem_wr_array[`S11_PS2]),
-     .addr(reg_addr_array[`S11_PS2]),
-     .rd_data(rd_data_array[`S11_PS2]),
-     .wr_data(wr_data_array[`S11_PS2]),
-     .ps2d(ps2d),
-     .ps2c(ps2c)
-     );
-     
-   // slot 12: ddfs 
-   chu_ddfs_core ddfs_slot12 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S12_DDFS]),
-    .read(mem_rd_array[`S12_DDFS]),
-    .write(mem_wr_array[`S12_DDFS]),
-    .addr(reg_addr_array[`S12_DDFS]),
-    .rd_data(rd_data_array[`S12_DDFS]),
-    .wr_data(wr_data_array[`S12_DDFS]),
-    .focw_ext(26'h0),
-    .pha_ext(26'h0),
-    .env_ext(adsr_env),
-    .pcm_out(),
-    .digital_out(ddfs_sq_wave),
-    .pdm_out(pdm)
-    );
     
-   // slot 13: adsr 
-   chu_adsr_core adsr_slot13 
-   (.clk(clk),
-    .reset(reset),
-    .cs(cs_array[`S13_ADSR]),
-    .read(mem_rd_array[`S13_ADSR]),
-    .write(mem_wr_array[`S13_ADSR]),
-    .addr(reg_addr_array[`S13_ADSR]),
-    .rd_data(rd_data_array[`S13_ADSR]),
-    .wr_data(wr_data_array[`S13_ADSR]),
-    .adsr_env(adsr_env)
-    );
 
    // assign 0's to all unused slot rd_data signals
    generate
       genvar i;
-      for (i=14; i<64; i=i+1) begin
+      for (i=4; i<64; i=i+1) begin
+         if ((i != 8) && (i != 10))
          assign rd_data_array[i] = 32'h0;
       end
    endgenerate
